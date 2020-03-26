4c760b56d8beeaec641f509254cb2226c6c7cbae
cv@napoleonit.ru <cv@napoleonit.ru>
2020-03-26 19:58:04 +0500

    added increment test

---
+++ app/src/test/java/com/example/vladimir_cher_shop/IncrementTest.kt
@@ -0,0 +1,19 @@
+package com.example.vladimir_cher_shop
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class IncrementTest {
+
+
+    @Test
+    fun incrementTest{
+
+        var i = 1
+        i += 1
+        assertEquals(2, 1)
+
+    }
+
+
+}
