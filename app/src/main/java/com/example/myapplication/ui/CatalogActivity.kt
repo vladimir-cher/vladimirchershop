cb2b44ad70e996e4f903f81556933b1c89d1e09f
maksim_novikov <rodgersdevelop@gmail.com>
2020-04-18 10:40:27 +0500

    add catalog activity add folding

---
+++ app/src/main/java/com/example/myapplication/ui/CatalogActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myapplication.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.catalog_layout.*
+
+class CatalogActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.catalog_layout)
+
+        Log.d(tag, "savedInstanceState = $savedInstanceState")
+        val savedInt = savedInstanceState?.getInt(SAVE_SATE_INT)
+        Log.d(tag, "savedInt $savedInt")
+
+        catalogCheckoutBtn.setOnClickListener {
+            val intent = Intent(this, CheckoutActivity::class.java).apply {
+                putExtra(PRODUCT_ID, 1000)
+            }
+            startActivityForResult(intent, REQUEST_AUTH)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(SAVE_SATE_INT, 89)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (REQUEST_AUTH == requestCode) {
+            val isUserAuth = data?.extras?.getBoolean(IS_USER_AUTH)
+            Log.d(tag, "onActivityResult ${isUserAuth.toString()}")
+        }
+    }
+
+    companion object {
+        const val PRODUCT_ID = "PRODUCT_ID"
+        const val REQUEST_AUTH: Int = 10
+        const val IS_USER_AUTH = "IS_USER_AUTH"
+        const val SAVE_SATE_INT = "SAVE_SATE_INT"
+    }
+}
